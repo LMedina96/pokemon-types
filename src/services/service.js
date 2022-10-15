ce97031d816ab16def795d4c8faf0e6951e819b6
LMedina96 <lucasgmedina96@gmail.com>
2022-10-15 16:36:25 -0300

    All types table without style created

--- src/services/service.js
+++ src/services/service.js
@@ -1,6 +1,27 @@
-function getPokemonTypes() {
-    //https://pokeapi.co/api/v2/type
-    fetch('https://pokeapi.co/api/v2/type')
-        .then(response => response.json())
-        .then(data => console.log(data));
+let getPokemonTypes = async () => {
+
+    let url = "https://pokeapi.co/api/v2/type";
+    const options = { method: "GET" };
+
+    await fetch(url, options)
+    .then((response) => response.json())
+    .then((response) => (data = response))
+    .catch((err) => console.error(err));
+
+    return data;
+};
+
+let buildTableAllTypes = async () => {
+    // let $tableAllTypes = document.getElementById('tableAllTypes');
+    let typesList = document.querySelector('#typesList')
+    let typeItem;
+
+    let response = await getPokemonTypes();
+
+    response.results.forEach((type) => {
+        typeItem = document.createElement('li')
+        typeItem.appendChild(document.createTextNode(type.name))
+
+        typesList.appendChild(typeItem)
+    })
 }
